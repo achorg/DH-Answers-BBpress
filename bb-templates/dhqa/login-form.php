a72b075b7d8292dc872561bc1e5f85ba56f0a776
Joseph Gilbert <jfg9x@harmonium.local>
2010-04-30 08:35:36 -0400

    login

--- bb-templates/dhqa/login-form.php
+++ bb-templates/dhqa/login-form.php
@@ -9,20 +9,20 @@
 	?>
 
 	</p>
-	<div class="span-3">
+	<div class="span-4">
 		<label><?php _e('Username'); ?></label>
-		<input name="user_login" type="text" id="quick_user_login" size="6" maxlength="40" value="<?php if (!is_bool($user_login)) echo $user_login; ?>" tabindex="1" />			
+		<input name="user_login" type="text" id="quick_user_login" size="8" maxlength="40" value="<?php if (!is_bool($user_login)) echo $user_login; ?>" tabindex="1" />			
 	</div>
-	<div class="span-3">
+	<div class="span-4 last">
 		<label><?php _e( 'Password' ); ?></label>
-		<input name="password" type="password" id="quick_password" size="6" maxlength="40" tabindex="2" />
+		<input name="password" type="password" id="quick_password" size="8" maxlength="40" tabindex="2" />
 	</div>
-	<div class="span-2 last">
+	<div class="span-4">
 		<input name="re" type="hidden" value="<?php echo $re; ?>" />
 		<?php wp_referer_field(); ?>
 		<input type="submit" name="Submit" class="submit" value="<?php echo esc_attr__( 'Log in &raquo;' ); ?>" tabindex="4" />
 	</div>
-	<div class="remember span-8 last">
+	<div class="remember span-4 last">
 		<input name="remember" type="checkbox" id="quick_remember" value="1" tabindex="3"<?php echo $remember_checked; ?> />
 		<label><?php _e('Remember me'); ?></label>
 	</div>
