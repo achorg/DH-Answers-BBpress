3eba04d6f8fc40e9a093e24b546ebbf82567dc16
Alliance of Digital Humanities Organizations <adhoadmin@woodward.joyent.us>
2010-06-23 16:57:58 +0000

    twitter notification on new topic, not old updates

--- bb-plugins/twitter-notification/twitter-notification.php
+++ bb-plugins/twitter-notification/twitter-notification.php
@@ -9,6 +9,11 @@
  */
  
 function tweet_new_topic($topic_id, $topic_title) {
+	global $bb_post;
+	if($bb_post){
+	// post already exists
+	}
+	else {
 	$t_title = get_topic_title($topic_id);
 	
 	// TODO: can we use a URL shoterner?
@@ -18,14 +23,14 @@ function tweet_new_topic($topic_id, $topic_title) {
 	if (strlen($message)>140) {$message = substr($message,0,139) . '…';}
 
 	// Set username and password
-	$username = 'adhoqatest';
-	$password = 'Answ3rs';
+	$username = '';
+	$password = '';
 
 	// do a simple command-line curl with the status
 	// TODO: check return status
 	 exec("curl -u $username:$password -d status=" . escapeshellarg($message) . " http://api.twitter.com/1/statuses/update.json", $output, $return);
-
+	}
 	return $topic_title;
 }
-add_action('bb_update_topic', 'tweet_new_topic', 10, 2);
+add_action('bb_insert_topic', 'tweet_new_topic', 10, 2);
 ?>
