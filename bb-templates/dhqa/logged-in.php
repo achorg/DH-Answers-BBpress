56742e303a58590a0a468ee8eec91a9c28e6e743
Joseph Gilbert <jfg9x@d-137-155-175.bootp.virginia.edu>
2010-06-23 16:00:35 -0400

    header buttons

--- bb-templates/dhqa/logged-in.php
+++ bb-templates/dhqa/logged-in.php
@@ -1,5 +1,3 @@
-<p class="login">
-	<?php printf(__('Welcome, %1$s'), bb_get_profile_link(bb_get_current_user_info( 'name' )));?>
-	<?php bb_admin_link( 'before= | ' );?>
-	| <?php bb_logout_link(); ?>
-</p>
+<span class="header-button profile-link"><?php printf(__('Welcome, %1$s'), bb_get_profile_link(bb_get_current_user_info( 'name' )));?></span>
+<span class="header-button admin-link"><?php bb_admin_link();?></span>
+<span class="header-button login-link"><?php bb_logout_link(); ?></span>
--- bb-templates/dhqa/login-form.php
+++ bb-templates/dhqa/login-form.php
@@ -1,12 +1,9 @@
 <form class="login" method="post" action="<?php bb_uri( 'bb-login.php', null, BB_URI_CONTEXT_FORM_ACTION + BB_URI_CONTEXT_BB_USER_FORMS ); ?>">
-	<p class="span-8 last">
 		<?php
 	printf(
-		__( '<span class="header-button"><a href="%1$s">Register</a></span> <span class="header-button"><a href="%2$s">Log in</a></span>' ),
+		__( '<span class="header-button register-link"><a href="%1$s">Sign up</a></span> <span class="header-button login-link"><a href="%2$s">Log in</a></span>' ),
 		bb_get_uri( 'register.php', null, BB_URI_CONTEXT_A_HREF + BB_URI_CONTEXT_BB_USER_FORMS ),
 		bb_get_uri( 'bb-login.php', null, BB_URI_CONTEXT_FORM_ACTION + BB_URI_CONTEXT_BB_USER_FORMS )
 	);
 	?>
-
-	</p>
 </form>
