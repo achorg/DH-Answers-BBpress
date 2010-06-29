de13d39cec0aa996a437e32481f49b9889bc7c28
Joseph Gilbert <adhoadmin@woodward.joyent.us>
2010-06-29 20:17:13 +0000

    settling on 2-col layout

--- bb-templates/dhqa/footer.php
+++ bb-templates/dhqa/footer.php
@@ -1,19 +1,7 @@
 		</div>
-	</div>
-	<div id="footer" role="contentinfo">
-		<p><?php printf(__('%1$s is proudly powered by <a href="%2$s">bbPress</a>.'), bb_option('name'), "http://bbpress.org") ?></p>
-
-		<!-- If you like showing off the fact that your server rocks -->
-		<!-- <p class="showoff">
-<?php
-global $bbdb;
-printf(
-__( 'This page generated in %s seconds, using %d queries.' ),
-bb_number_format_i18n( bb_timer_stop(), 2 ),
-bb_number_format_i18n( $bbdb->num_queries )
-);
-?>
-		</p> -->
+			<div id="footer" role="contentinfo" class="span-24">
+				<p><?php printf(__('%1$s is proudly powered by <a href="%2$s">bbPress</a>.'), bb_option('name'), "http://bbpress.org") ?></p>
+			</div>
 	</div>
 
 <?php do_action('bb_foot'); ?>
