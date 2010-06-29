7f587b69a4fbbc7735f65d25d4f60c3cb318b448
Joseph Gilbert <adhoadmin@woodward.joyent.us>
2010-06-29 20:17:16 +0000

    ask question button

--- bb-templates/dhqa/logged-in.php
+++ bb-templates/dhqa/logged-in.php
@@ -1,3 +1,4 @@
-<span class="header-button profile-link"><?php printf(__('Welcome, %1$s'), bb_get_profile_link(bb_get_current_user_info( 'name' )));?></span>
+<span class="header-button ask-link"><?php bb_new_topic_link('Ask a Question'); ?></span>
+<span class="header-button profile-link"><?php printf(__('%1$s\'s Profile'), bb_get_profile_link(bb_get_current_user_info( 'name' )));?></span>
 <span class="header-button admin-link"><?php bb_admin_link();?></span>
 <span class="header-button login-link"><?php bb_logout_link(); ?></span>
