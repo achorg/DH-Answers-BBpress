e9d96e1b4fd537b2d9847c4858d6382377701a2e
Joseph Gilbert <adhoadmin@woodward.joyent.us>
2010-06-29 20:17:16 +0000

    login tweaks

--- bb-templates/dhqa/login-form.php
+++ bb-templates/dhqa/login-form.php
@@ -2,7 +2,7 @@
 	<p class="span-8 last">
 		<?php
 	printf(
-		__( '<a href="%1$s">Register</a> or log in - <a href="%2$s">lost password?</a>' ),
+		__( '<span class="header-button"><a href="%1$s">Register</a></span> <span class="header-button"><a href="%2$s">Log in</a></span>' ),
 		bb_get_uri( 'register.php', null, BB_URI_CONTEXT_A_HREF + BB_URI_CONTEXT_BB_USER_FORMS ),
 		bb_get_uri( 'bb-login.php', null, BB_URI_CONTEXT_FORM_ACTION + BB_URI_CONTEXT_BB_USER_FORMS )
 	);
