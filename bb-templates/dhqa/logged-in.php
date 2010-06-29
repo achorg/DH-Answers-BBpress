388e04d587df8d48beb1a55cafafbfd647f5769d
Joseph Gilbert <adhoadmin@woodward.joyent.us>
2010-06-29 20:17:17 +0000

    profile link fix

--- bb-templates/dhqa/logged-in.php
+++ bb-templates/dhqa/logged-in.php
@@ -1,4 +1,4 @@
 <span class="header-button ask-link"><?php bb_new_topic_link('Ask a Question'); ?></span>
-<span class="header-button profile-link"><?php bb_get_profile_link('My Profile' );?></span>
+<span class="header-button profile-link"><?php bb_profile_link('My Profile'); ?></span>
 <span class="header-button admin-link"><?php bb_admin_link();?></span>
 <span class="header-button login-link"><?php bb_logout_link(); ?></span>
