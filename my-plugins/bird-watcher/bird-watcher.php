d8a8c8c34c34bbdbf4e1c3728249374d53d0dbbc
Joseph Gilbert <jfg9x@d-137-155-159.bootp.virginia.edu>
2010-08-06 14:40:57 -0400

    new strategy for bird watcher

--- my-plugins/bird-watcher/bird-watcher.php
+++ my-plugins/bird-watcher/bird-watcher.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Bird Watcher
- * Plugin Description: Create new questions from tweets with #askDH hashtag 
+ * Plugin Description: Create new questions from tweets with #ask hashtag 
  * Author: Joseph Gilbert
  * Author URI: http://lib.virginia.edu/scholarslab/
  * Plugin URI: http://lib.virginia.edu/scholarslab/
@@ -9,13 +9,31 @@
  */
 
 function get_tweets($hashtag) {
-	$tweets = array();
-	$feed = simplexml_load_file('http://search.twitter.com/search.rss?q=%23askdh'); 
+	//retrieve feed via simplexml
+	if($xml = simplexml_load_file('http://search.twitter.com/search.rss?q=%23'.$hashtag,'SimpleXMLElement', LIBXML_NOCDATA)) {
+		//find title, author, and tweet id with xpath
+		$result["title"] = $xml->xpath("/rss/channel/item/title");
+		$result["author"] = $xml->xpath("/rss/channel/item/author");
+		$result["guid"] = $xml->xpath("/rss/channel/item/guid");
+		//assign tweets and attributes to 2d array (e.g., author of first tweet will be $tweet[0]["author"])
+		foreach($result as $key => $attribute) {
+			$i=0;
+	    	foreach($attribute as $element) {
+	      		$tweet[$i][$key] = (string)$element;
+	      		$i++;
+	    	}
+	  	}
+		return $tweet;
+	} else {
+	  return false;
+	}
+}
 
-    foreach($feed->channel->item as $tweet) {
-		//publish unless it's already in the db
-		echo('<p>'.$tweet->guid.'</p>');
-    }
+function show_tweets($hashtag) {
+	$tweet = get_tweets($hashtag);
+	foreach($tweet as $msg) {
+		echo("<p>".$msg["title"]."</p>");
+	}
 }
 
 ?>
