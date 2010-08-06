6603b3d542c93dbe6b93b8054b6e0b5b935385e3
Joseph Gilbert <jfg9x@d-137-155-159.bootp.virginia.edu>
2010-08-06 15:04:26 -0400

    bird watcher retrieving tweets.  next: make posts.

--- my-plugins/bird-watcher/bird-watcher.php
+++ my-plugins/bird-watcher/bird-watcher.php
@@ -12,27 +12,17 @@ function get_tweets($hashtag) {
 	//retrieve feed via simplexml
 	if($xml = simplexml_load_file('http://search.twitter.com/search.rss?q=%23'.$hashtag,'SimpleXMLElement', LIBXML_NOCDATA)) {
 		//find title, author, and tweet id with xpath
-		$result["title"] = $xml->xpath("/rss/channel/item/title");
-		$result["author"] = $xml->xpath("/rss/channel/item/author");
-		$result["guid"] = $xml->xpath("/rss/channel/item/guid");
-		//assign tweets and attributes to 2d array (e.g., author of first tweet will be $tweet[0]["author"])
-		foreach($result as $key => $attribute) {
-			$i=0;
-	    	foreach($attribute as $element) {
-	      		$tweet[$i][$key] = (string)$element;
-	      		$i++;
-	    	}
-	  	}
-		return $tweet;
+		$tweets = $xml->xpath("/rss/channel/item");
+		return $tweets;
 	} else {
-	  return false;
+		return false;
 	}
 }
 
 function show_tweets($hashtag) {
-	$tweet = get_tweets($hashtag);
-	foreach($tweet as $msg) {
-		echo("<p>".$msg["title"]."</p>");
+	$tweets = get_tweets($hashtag);
+	foreach($tweets as $tweet) {
+		echo("<p>".$tweet->title."</p>");
 	}
 }
 
