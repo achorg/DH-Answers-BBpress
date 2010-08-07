65a2f1044d5aa07d40cb97511fee7e56f679d86b
Joseph Gilbert <jfg9x@harmonium.local>
2010-08-06 21:30:28 -0400

    bw duplication check in place, tag transfer, etc.

--- my-plugins/bird-watcher/bird-watcher.php
+++ my-plugins/bird-watcher/bird-watcher.php
@@ -24,22 +24,47 @@ function bw_add_tweets() {
 	$hashtag = 'askdh';
 	$tweets = bw_get_tweets($hashtag);
 	foreach($tweets as $tweet) {
+		
+		//extract tags from tweet
+		$full_tweet = (string)$tweet->title;
+		preg_match_all('/#[a-zA-Z0-9_-]+/',$full_tweet,$tagMatches);
+		$tag = array();
+		$tag_count = 0;
+		foreach($tagMatches as $tagMatch) {
+			$tag[$tag_count] = str_replace('#','',$tagMatch);
+			$full_tweet = str_replace($tagMatch, '', $full_tweet);
+		}
+		
+		//shorten title if need be
+		if(strlen($full_tweet) >= 45) {
+			$short_title = substr($full_tweet, 0, 44) . '[...]';
+		} else {
+			$short_title = $full_tweet;
+		}
+		
+		//get tweet guid to help with duplication prevention
 		preg_match('/\/([0-9]+)$/', $tweet->guid, $idMatch);
+		$tweetId = (int)$idMatch[1];
+		
 		//if topic doesn't already exist
-		if (!get_topic($idMatch[1])) {
-			bb_insert_topic(array(
-				'topic_id' => $idMatch[1],
-				'topic_title' => $tweet->title,
-				'topic_slug' => '',
+		if (bw_check_duplicate($tweetId)) {
+			//add a new topic by "Twitter User"
+			$new_topic = bb_insert_topic(array(
+				'topic_title' => str_replace('#askdh','',$short_title),
 				'topic_poster' => 1, // accepts ids
 				'topic_poster_name' => 'Twitter User', // accept names
 				'topic_last_poster' => 1, // accepts ids
 				'topic_last_poster_name' => 'Twitter User', // accept names
-				'topic_open' => 1,
-				'forum_id' => 'general' // accepts ids or slugs
+				'forum_id' => 'general', // accepts ids or slugs
+				'tags' => $tag
 			));
+			bb_update_topicmeta($new_topic,'tweetid',$tweetId);
 		}
 	}
+}
+
+function bw_check_duplicate($id) {
+	return($bbdb->get_var("SELECT COUNT(*) FROM $bbdb->topics AS topics INNER JOIN $bbdb->meta AS meta ON topics.topic_id=meta.object_id WHERE object_type = 'bb_topic' AND meta_key = 'twitterid' AND meta_value = $id") > 0);
 }
 
 //Check for new tweeted messages every time footer loads.  Better ideas?
