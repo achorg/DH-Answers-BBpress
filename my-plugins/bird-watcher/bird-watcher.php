f927d8f0c6aa03316e4d680e333d7705271875bf
Joseph Gilbert <jfg9x@d-137-155-159.bootp.virginia.edu>
2010-08-09 15:17:43 -0400

    tag match case insensitive.  working?

--- my-plugins/bird-watcher/bird-watcher.php
+++ my-plugins/bird-watcher/bird-watcher.php
@@ -27,12 +27,14 @@ function bw_add_tweets() {
 		
 		//extract tags from tweet
 		$full_tweet = (string)$tweet->title;
+		$tagMatches = array();
 		preg_match_all('/#[a-zA-Z0-9_-]+/',$full_tweet,$tagMatches);
 		$tag = array();
 		$tag_count = 0;
 		foreach($tagMatches as $tagMatch) {
 			$tag[$tag_count] = str_replace('#','',$tagMatch);
 			$full_tweet = str_replace($tagMatch, '', $full_tweet);
+			$tag_count++;
 		}
 		
 		//shorten title if need be
@@ -50,10 +52,10 @@ function bw_add_tweets() {
 		if (bw_check_duplicate($tweetId) == 0) {
 			//add a new topic by "Twitter User"
 			$new_topic = bb_insert_topic(array(
-				'topic_title' => str_replace('#askdh','',$short_title),
-				'topic_poster' => 1, // accepts ids
+				'topic_title' => str_ireplace('#askdh','',$short_title),
+				'topic_poster' => 13, // accepts ids
 				'topic_poster_name' => 'Twitter User', // accept names
-				'topic_last_poster' => 1, // accepts ids
+				'topic_last_poster' => 13, // accepts ids
 				'topic_last_poster_name' => 'Twitter User', // accept names
 				'forum_id' => 'general', // accepts ids or slugs
 				'tags' => $tag
@@ -66,6 +68,8 @@ function bw_add_tweets() {
 function bw_check_duplicate($id) {
 	global $bbdb;
 	//check if Twitter guid exists in bb_meta table
+	//note that we aren't checking to see if the post is 
+	//"normal" or not to prevent deleted posts from returning
 	return($bbdb->query("SELECT * FROM $bbdb->topics AS topics LEFT JOIN $bbdb->meta AS meta ON topics.topic_id=meta.object_id WHERE object_type = 'bb_topic' AND meta_key = 'tweetid' AND meta_value='$id'"));
 }
 
