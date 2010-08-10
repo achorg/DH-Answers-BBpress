31de46144a6ac718bcd87cf3bc93ca6df8cd6b65
Joseph Gilbert <jfg9x@d-137-155-159.bootp.virginia.edu>
2010-08-10 11:31:54 -0400

    bw: no topic if mention, tag function, code cleanup

--- my-plugins/bird-watcher/bird-watcher.php
+++ my-plugins/bird-watcher/bird-watcher.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Bird Watcher
- * Plugin Description: Create new questions from tweets with #askDH hashtag 
+ * Plugin Description: Create new questions from tweets with #DHanswers hashtag 
  * Author: Joseph Gilbert
  * Author URI: http://lib.virginia.edu/scholarslab/
  * Plugin URI: http://lib.virginia.edu/scholarslab/
@@ -21,48 +21,50 @@ function bw_get_tweets($hashtag) {
 
 function bw_add_tweets() {
 	
-	$hashtag = 'askdh';
+	$hashtag = 'dhanswers';
 	$tweets = bw_get_tweets($hashtag);
 	foreach($tweets as $tweet) {
-		
-		//extract tags from tweet
+		//ignore if tweet contains a mention (@whatever)
 		$full_tweet = (string)$tweet->title;
-		$tagMatches = array();
-		preg_match_all('/#[a-zA-Z0-9_-]+/',$full_tweet,$tagMatches);
-		$tag = array();
-		$tag_count = 0;
-		foreach($tagMatches as $tagMatch) {
-			$tag[$tag_count] = str_replace('#','',$tagMatch);
-			$full_tweet = str_replace($tagMatch, '', $full_tweet);
-			$tag_count++;
-		}
+		if(!preg_match('/@[\w_-]+/',$full_tweet)){
+			
+			//extract tags and remove from tweet
+			$tags = bw_get_tags($full_tweet);
+			$full_tweet = preg_replace('/#[\w_-]+/', '', $full_tweet);
 		
-		//shorten title if need be
-		if(strlen($full_tweet) >= 45) {
-			$short_title = substr($full_tweet, 0, 44) . '[...]';
-		} else {
-			$short_title = $full_tweet;
-		}
+			//shorten title if need be
+			if(strlen($full_tweet) >= 45) {
+				$short_title = substr($full_tweet, 0, 44) . '[...]';
+			} else {
+				$short_title = $full_tweet;
+			}
 		
-		//get tweet guid to help with duplication prevention
-		//find string position of last slash then get substring after it
-		$tweetId = substr($tweet->guid, strrpos($tweet->guid,'/')+1);
+			//get tweet guid to help with duplication prevention
+			//find string position of last slash then get substring after it
+			$tweetId = substr($tweet->guid, strrpos($tweet->guid,'/')+1);
 				
-		//if topic doesn't already exist
-		if (bw_check_duplicate($tweetId) == 0) {
-			//add a new topic by "Twitter User"
-			$new_topic = bb_insert_topic(array(
-				'topic_title' => str_ireplace('#askdh','',$short_title),
-				'topic_poster' => 13, // accepts ids
-				'topic_poster_name' => 'Twitter User', // accept names
-				'topic_last_poster' => 13, // accepts ids
-				'topic_last_poster_name' => 'Twitter User', // accept names
-				'forum_id' => 'general', // accepts ids or slugs
-				'tags' => $tag
-			));
-			bb_update_topicmeta($new_topic,'tweetid',$tweetId);
-		}
-	}
+			//if topic doesn't already exist (i.e., there are zero posts with that tweetid)
+			if (!bw_check_duplicate($tweetId)) {
+				//add a new topic by "Twitter User"
+				$new_topic = bb_insert_topic(array(
+					'topic_title' => str_ireplace('#dhanswers','',$short_title),
+					'topic_poster' => 13, // accepts ids
+					'topic_last_poster' => 13, // accepts ids
+					'forum_id' => 'general', // accepts ids or slugs
+					'tags' => $tags
+				));
+				//add the tweet guid to the meta table for duplication
+				bb_update_topicmeta($new_topic,'tweetid',$tweetId);
+				//add a new post to this topic with the full tweet
+				bb_insert_post(array(
+					'topic_id' => $new_topic,
+					'post_text' => $full_tweet,
+					'poster_id' => 13, // accepts ids or names
+					'poster_ip' => '127.0.0.1'
+				));
+			} //end if bw_check_duplicate
+		} //end if preg_match mentions
+	} //end foreach tweets
 }
 
 function bw_check_duplicate($id) {
@@ -71,6 +73,20 @@ function bw_check_duplicate($id) {
 	//note that we aren't checking to see if the post is 
 	//"normal" or not to prevent deleted posts from returning
 	return($bbdb->query("SELECT * FROM $bbdb->topics AS topics LEFT JOIN $bbdb->meta AS meta ON topics.topic_id=meta.object_id WHERE object_type = 'bb_topic' AND meta_key = 'tweetid' AND meta_value='$id'"));
+}
+
+function bw_get_tags($tweet) {
+	//extract tags from tweet
+	$tagMatches = array();
+	preg_match_all('/#[\w_-]+/',$tweet,$tagMatches);
+	$tags = array();
+	
+	$tag_count = 0;
+	foreach($tagMatches as $tagMatch) {
+		$tags[$tag_count] = str_replace('#','',$tagMatch);
+		$tag_count++;
+	}
+	return $tags;
 }
 
 //Check for new tweeted messages every time footer loads.  Better ideas?
