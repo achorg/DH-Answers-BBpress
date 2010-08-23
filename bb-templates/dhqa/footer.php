04a004aa7a26a18c43a64a278e0a2f44e294ad14
Joseph Gilbert <jfg9x@d-137-155-159.bootp.virginia.edu>
2010-08-23 10:46:12 -0400

    footer lang tweaks

--- bb-templates/dhqa/footer.php
+++ bb-templates/dhqa/footer.php
@@ -1,9 +1,9 @@
 		</div>
 			<div id="footer" role="contentinfo" class="span-24">
-				<p id="lang-switch">
+				<div id="lang-switch">
 					<span class="lang-label"><?php _e('Language'); ?></span>
 					<?php do_action('bb_language_switcher',''); ?>
-				</p>			
+				</div>			
 				<p>A project of the <a href="http://www.ach.org">Association for Computers and the Humanities</a> and the Chronicle of Higher Education's <a href="http://profhacker.com">ProfHacker</a></p>
 				<p>Icons by <a href="http://www.famfamfam.com/lab/icons/silk/">famfamfam</a></p>
 				<p><a href="http://lib.virginia.edu/scholarslab"><img src="<?php echo(bb_active_theme_uri().'images/slab-logo.jpg'); ?>" alt="Scholars' Lab" title="Scholars' Lab"/></a></p>
