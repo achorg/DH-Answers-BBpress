ba6cc0268b7d0548156ace39b5ff8247c9e3efa5
Joseph Gilbert <jfg9x@harmonium.local>
2010-08-23 19:09:47 -0400

    playing with simpletest

--- my-plugins/bird-watcher/test/bird-watcher-test.php
+++ my-plugins/bird-watcher/test/bird-watcher-test.php
@@ -4,12 +4,12 @@ require_once('../bird-watcher.php');
   
 class TestBirdWatcher extends UnitTestCase {  
 
-	function test_get_tweets() {
+	function testGetTweets() {
 		$test_tweets = bw_get_tweets( 'test' );
 		$this->assertIsA( $test_tweets , 'array' );
 	}
 	
-	function test_tweet_content()	{
+	function testTweetContent()	{
 		$test_content = bw_get_tweets( 'test' );
 		foreach( $test_content as $content ) {
 			$this->assertIsA( $content->title, 'string' );
