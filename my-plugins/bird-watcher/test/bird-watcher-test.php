1f34685253eecdb7abf11f988737710f0fbd6ebc
Joseph Gilbert <jfg9x@d-172-25-247-105.dhcp.virginia.edu>
2010-08-23 15:29:05 -0400

    simpletest for bw

---
+++ my-plugins/bird-watcher/test/bird-watcher-test.php
@@ -0,0 +1,26 @@
+<?php  
+require_once(dirname(__FILE__) . '/simpletest/autorun.php');  
+require_once('../bird-watcher.php');  
+  
+class TestBirdWatcher extends UnitTestCase {  
+
+	function test_get_tweets() {
+		$test_tweets = bw_get_tweets( 'test' );
+		$this->assertIsA( $test_tweets ), 'array' );
+	}
+	
+	function test_tweet_content()	{
+		$test_content = bw_get_tweets( 'test' );
+		foreach( $test_content as $content ) {
+			$this->assertIsA( $content->title, 'string' );
+			$this->assertIsA( bw_get_tweet_user( $content->author ), 'string' );		
+			$this->assertIsA( bw_get_tweet_id( $content->guid ), 'string' );
+		}
+	}
+	
+	// test these functions
+	// !bw_check_duplicate( $tweet_id ) && !bw_has_mention( $tweet_text ) && bw_is_user( $tweet_user
+  
+}
+
+?>
