618651b14956bb86d1e3702fbd63af4f00882821
Joseph Gilbert <jfg9x@d-137-155-159.bootp.virginia.edu>
2010-09-02 11:44:37 -0400

    misplaced div

--- bb-templates/dhqa/header.php
+++ bb-templates/dhqa/header.php
@@ -48,20 +48,20 @@ if ( bb_is_profile() ) {
 </head>
 <body id="<?php bb_location(); ?>">
 	<div class="container prepend-top append-bottom">
+		<div id="util-login">
 		<?php if ( !bb_is_user_logged_in() ) { ?>
-			<div id="util-login">
 				<?php printf(
 					__( '<a href="%2$s">Log in</a> | <a href="%1$s">Register</a>' ),
 					bb_get_uri( 'register.php', null, BB_URI_CONTEXT_A_HREF + BB_URI_CONTEXT_BB_USER_FORMS ),
 					bb_get_uri( 'bb-login.php', null, BB_URI_CONTEXT_FORM_ACTION + BB_URI_CONTEXT_BB_USER_FORMS )
 				); ?>
-			</div>
 		<?php } else { ?>
 			<?php if($bb_current_user->has_cap('administrate') || $bb_current_user->has_cap('moderate')) { ?>
 			<?php bb_admin_link();?>
 			<?php } ?>
 			<?php bb_logout_link(); ?>			
 		<?php }?>
+		</div>
 		<div id="header" role="banner" class="prepend-6 span-18">
 			<a id="ach-logo" href="http://www.ach.org">ACH</a>
 			<h1><a href="<?php bb_uri(); ?>"><?php bb_option('name'); ?></a></h1>
