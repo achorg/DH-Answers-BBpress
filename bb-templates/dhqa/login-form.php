9a0346383fd2bed0ce0dabfd1d4a1bcf90f0c5b1
Joseph Gilbert <jfg9x@d-172-25-73-196.bootp.virginia.edu>
2010-09-09 13:29:09 -0400

    slightly more valid--best answer link test

--- bb-templates/dhqa/login-form.php
+++ bb-templates/dhqa/login-form.php
@@ -1,7 +1,7 @@
 <form class="login" method="post" action="<?php bb_uri( 'bb-login.php', null, BB_URI_CONTEXT_FORM_ACTION + BB_URI_CONTEXT_BB_USER_FORMS ); ?>">
 		<?php
 	printf(
-		__( '<span class="header-button ask-link"><a href="%2$s">Ask a Question</a></span>' ),
+		__( '<div class="header-button ask-link"><a href="%2$s">Ask a Question</a></div>' ),
 		bb_get_uri( 'register.php', null, BB_URI_CONTEXT_A_HREF + BB_URI_CONTEXT_BB_USER_FORMS ),
 		bb_get_uri( 'bb-login.php', null, BB_URI_CONTEXT_FORM_ACTION + BB_URI_CONTEXT_BB_USER_FORMS )
 	);
