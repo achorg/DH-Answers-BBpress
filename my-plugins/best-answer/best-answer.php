e6153e91771aee4ad04d22fbe124f15956fd5713
Joseph Gilbert <jfg9x@d-137-155-158.bootp.virginia.edu>
2010-09-09 14:45:02 -0400

    best answer meta link

--- my-plugins/best-answer/best-answer.php
+++ my-plugins/best-answer/best-answer.php
@@ -92,8 +92,11 @@ function best_answer_head() {global $best_answer; echo '<style type="text/css">'
 
 function best_answer_meta() {
 	global $topic;
-	if (!empty($topic->best_answer)) {
-		echo "<li class='best_answer_meta'>This question has a best answer!</li>";
+	if ( !empty( $topic->best_answer ) ) {
+		if (!is_array($topic->best_answer)) {
+			(array) $best_post = explode( ',', $topic->best_answer );
+		}
+		echo "<li class='best_answer_meta'>This question has a <a href='#post-" . $best_post[0]  . "'>best answer</a>.</li>";
 	}
 }
 
