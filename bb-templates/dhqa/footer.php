ccde1f0142a8232fe2dc212e625d0f5f87c9e5b3
Joseph Gilbert <jfg9x@harmonium.local>
2010-09-29 07:58:30 -0400

    footer tweak

--- bb-templates/dhqa/footer.php
+++ bb-templates/dhqa/footer.php
@@ -6,7 +6,7 @@
 				</div>			
 				<p>A project of the <a href="http://www.ach.org">Association for Computers and the Humanities</a> and the Chronicle of Higher Education's <a href="http://profhacker.com">ProfHacker</a></p>
 				<p>Icons by <a href="http://www.famfamfam.com/lab/icons/silk/">famfamfam</a></p>
-				<p><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>.</p>
+				<p><a style="float: right; margin-left: 5px;" rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Creative Commons License" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>.</p>
 				<p><a href="http://lib.virginia.edu/scholarslab"><img src="<?php echo(bb_active_theme_uri().'images/slab-logo.jpg'); ?>" alt="Scholars' Lab" title="Scholars' Lab"/></a></p>
 			</div>
 	</div>
