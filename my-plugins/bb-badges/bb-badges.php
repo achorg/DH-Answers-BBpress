f43b1b61b8203be47a2899889ff8fce2c45cc1cf
Wayne <wayne.graham@gmail.com>
2010-10-05 10:14:56 -0400

    Refactored and simplified conditional logic to assign badge classes; rewrote queries to tokenize inputs

--- my-plugins/bb-badges/bb-badges.php
+++ my-plugins/bb-badges/bb-badges.php
@@ -8,6 +8,11 @@
  * Version: 0.1
  */
 
+/**
+ * Display the array of badges a user has amassed
+ *
+ * @return void
+ */
 function show_badges() {
 	$badges = get_badges(get_post_author_id());
 	// Print array of badges
@@ -16,52 +21,86 @@ function show_badges() {
 	}
 }
 
+/**
+ * Calculate the social badges earned by a specific user
+ *
+ * @param $user integer ID of the user to look up.
+ * @return $badges array Array of social badges
+ */
 function get_badges($user) {
 	// initiate badge array
-	$badges = array();
+	$badges = array(2);
 	
 	// Get # of user posts, # of best answers
 	$post_count = get_post_count($user);
 	$answer_count = get_answer_count($user);
 	
 	// Assign post count badge
-	if($post_count >= 50) {
-		$badges["posts"] = "<span title='50+ Posts' class='badge gold-poster ".$post_count."'><!-- 50+ posts! --></span>";
-	} elseif($post_count < 50 && $post_count >= 20) {
-		$badges["posts"] = "<span title='20+ Posts' class='badge silver-poster ".$post_count."'><!-- 20-49 posts --></span>";
-	} elseif($post_count < 20 && $post_count >= 5) {
-		$badges["posts"] = "<span title='5+ Posts' class='badge bronze-poster ".$post_count."'><!-- 10-19 posts --></span>";
-	} else {
-		$badges["posts"] = "<span class='badge ".$post_count."'><!-- fewer than 5 posts --></span>";
+	switch($post_count)
+	{
+	    case($post_count >= 50):
+	        $badges["posts"] = "<span title='50+ Posts' class='badge gold-poster ".$post_count."'><!-- 50+ posts! --></span>";
+	        break;
+	    case($post_count >= 20):
+	       	$badges["posts"] = "<span title='20+ Posts' class='badge silver-poster ".$post_count."'><!-- 20-49 posts --></span>";
+	       	break;
+	    case($post_count >= 5):
+	        $badges["posts"] = "<span title='5+ Posts' class='badge bronze-poster ".$post_count."'><!-- 10-19 posts --></span>";
+	        break;
+	    default:
+	        $badges["posts"] = "<span class='badge ".$post_count."'><!-- fewer than 5 posts --></span>";
 	}
-	
+
 	// Assign answer count badge
-	if($answer_count >= 25) {
-		$badges["answers"] = "<span title='25 Best Answers' class='badge gold-answerer ".$answer_count."'><!-- 25+ answers! --></span>";
-	} elseif($answer_count < 25 && $answer_count >= 10) {
-		$badges["answers"] = "<span title='10+ Best Answers' class='badge silver-answerer ".$answer_count."'><!--10-24 answers --></span>";
-	} elseif($answer_count < 10 && $answer_count >= 3) {
-		$badges["answers"] = "<span title='3+ Best Answers' class='badge bronze-answerer ".$answer_count."'><!-- 3-9 answers --></span>";
-	} else {
-		$badges["answers"] = "<span class='badge ".$answer_count."'><!-- fewer than 3 answers --></span>";
+	switch($answer_count)
+	{
+	    case($answer_count >= 25):
+	        $badges["answers"] = "<span title='25 Best Answers' class='badge gold-answerer ".$answer_count."'><!-- 25+ answers! --></span>";
+	        break;
+	    case($answer_count >= 10):
+	        $badges["answers"] = "<span title='10+ Best Answers' class='badge silver-answerer ".$answer_count."'><!--10-24 answers --></span>";
+	        break;
+	    case($answer_count >= 3):
+	        $badges["answers"] = "<span title='3+ Best Answers' class='badge bronze-answerer ".$answer_count."'><!-- 3-9 answers --></span>";
+	        break;
+	    default:
+	        $badges["answers"] = "<span class='badge ".$answer_count."'><!-- fewer than 3 answers --></span>";
 	}
 	
 	// Return array of badges
 	return $badges;
 }
 
+
+/**
+ * Returns the number of posts for a user
+ * @param $user integer user
+ */
 function get_post_count($user) {
 	global $bbdb;
-	return $bbdb->query("SELECT * FROM $bbdb->posts WHERE poster_id = $user AND post_status = 0");
+	return $bbdb->query(sprintf("SELECT COUNT(*) FROM $bbdb->posts WHERE poster_id = '%d' AND post_status = 0", $user));
 }
 
+/**
+ * Return the number of best answers for a user
+ * @param $user integer 
+ */
 function get_answer_count($user) {
 	global $bbdb;
-<<<<<<< HEAD
-	return $bbdb->query("SELECT * FROM $bbdb->posts AS posts LEFT JOIN $bbdb->meta AS meta ON posts.topic_id=meta.object_id WHERE object_type = 'bb_topic' AND poster_id = $user AND post_status = 0 AND meta_key = 'best_answer' AND post_position = meta_value");
-=======
-	return $bbdb->query("SELECT * FROM $bbdb->posts AS posts LEFT JOIN $bbdb->meta AS meta ON posts.topic_id=meta.object_id WHERE object_type = 'bb_topic' AND poster_id = $user AND post_status = 0 AND meta_key = 'best_answer' AND post_id = meta_value");
->>>>>>> 29a604a95344f87ef6e2049b84c61cec32001d76
+	
+	$sql = <<<SQL
+	SELECT COUNT(*) 
+	FROM $bbdb->posts AS p
+	LEFT JOIN $bbdb->meta AS m
+	    ON p.topic_id = m.object_id
+	WHERE m.object_type = 'bb_topic' 
+	AND m.meta_key = 'best_answer'
+	AND p.post_status = 0
+	AND p.post_id = m.meta_value
+	AND p.poster_id = '%d'
+SQL;
+
+	return $bbdb->query(sprintf($sql), $user));
 }
 
 ?>
